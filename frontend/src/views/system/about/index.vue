1d709c99bee9878d906cd6387c6a77a3f504c832
zl_liuhaiyong <haiyong.liu@transwarp.cn>
2023-03-16 14:47:32 +0800

    修改 关于中的license期限提示

--- frontend/src/views/system/about/index.vue
+++ frontend/src/views/system/about/index.vue
@@ -7,38 +7,52 @@
       <div class="license-content">
         <div v-if="license.status === 'Fail'">{{ $t('about.invalid_license') }}</div>
         <div v-if="license.status !== 'Fail'">
-          <table>
-            <tr>
-              <th>{{ $t('about.auth_to') }}</th>
-              <td>{{ license.corporation }}</td>
-            </tr>
-            <tr>
-              <th>{{ $t('about.expiration_time') }}</th>
-              <td>
+          <el-alert
+            v-if="license.status === 'valid' && dateDecide"
+            :title="`您的授权将在 ${manyDays} 后的 ${license.expired} 过期，为了不影响使用，请及时续费!`"
+            type="info"
+            show-icon
+            style="margin-bottom: 10px;"
+            :closable="false">
+          </el-alert>
+          <el-alert
+            v-if="license.status === 'expired'"
+            title="您的授权已将过期，请及时续费!"
+            type="error"
+            show-icon
+            style="margin-bottom: 10px;"
+            :closable="false">
+          </el-alert>
+          <el-row :style="{'margin-top': !dateDecide? '30px': '0px;'}">
+            <el-col class="col_box">
+              <el-col :span="10" class="box_title">{{ $t('about.auth_to') }}：</el-col>
+              <el-col :span="14">{{ license.corporation }}</el-col>
+            </el-col>
+            <el-col class="col_box">
+              <el-col :span="10" class="box_title">{{ $t('about.expiration_time') }}：</el-col>
+              <el-col :span="14">
                 <label v-if="license.status === 'expired'" style="color: red">{{ license.expired }} {{ $t('about.expirationed') }}</label>
                 <label v-if="license.status === 'valid'">{{ license.expired }}</label>
-              </td>
-            </tr>
-            <tr>
-              <th>{{ $t('about.auth_num') }}</th>
-              <td>{{ license.count }}</td>
-            </tr>
-            <tr>
-              <th>{{ $t('about.version') }}</th>
-              <td>
+              </el-col>
+            </el-col>
+            <el-col class="col_box">
+              <el-col :span="10" class="box_title">{{ $t('about.auth_num') }}：</el-col>
+              <el-col :span="14">{{ license.count }}</el-col>
+            </el-col>
+            <el-col class="col_box">
+              <el-col :span="10" class="box_title">{{ $t('about.version') }}：</el-col>
+              <el-col :span="14">
                 <span v-if="license.edition">
                   <span v-if="license.edition === 'Standard'">{{ $t('about.standard') }}</span>
                   <span v-if="license.edition === 'Enterprise'">{{ $t('about.enterprise') }}</span>
                 </span>
-              </td>
-            </tr>
-            <tr>
-              <th>{{ $t('about.version_num') }}</th>
-              <td>
-                <span>{{ build }}</span>
-              </td>
-            </tr>
-          </table>
+              </el-col>
+            </el-col>
+            <el-col class="col_box">
+              <el-col :span="10" class="box_title">{{ $t('about.version_num') }}：</el-col>
+              <el-col :span="14">{{ build }}</el-col>
+            </el-col>
+          </el-row>
         </div>
 
         <div class="md-padding" />
@@ -82,7 +96,23 @@ export default {
   computed: {
     ...mapGetters([
       'user'
-    ])
+    ]),
+    manyDays() {
+      let num = Math.ceil((new Date(this.license.expired).getTime() - new Date().getTime()) / (1000*3600*24))
+      // console.log('几天',num)
+      return num +'天'
+    },
+    dateDecide(){
+      if(this.license.expired) {
+        if((new Date(this.license.expired+' 00:00:00').getTime() - new Date().getTime()) > 1000*3600*24*7) {
+          return false
+        } else {
+          return true
+        }
+      } else {
+        return false
+      }
+    },
   },
   mounted() {
     this.$nextTick(() => {
@@ -95,7 +125,6 @@ export default {
     this.getLicenseInfo()
   },
   methods: {
-
     initVersion() {
       buildVersion().then(res => {
         this.build = res.data
@@ -107,6 +136,7 @@ export default {
     //     console.log(this.license)
     //   })
       this.validateHandler({}, res => {
+        console.log('res.data',res.data)
         this.license = this.getLicense(res.data)
       })
     },
@@ -140,6 +170,7 @@ export default {
         if (response.data.status === 'valid') {
           this.$success(this.$t('about.update_success'))
           this.license = this.getLicense(response.data)
+          // console.log('license数据',this.license)
         } else {
           this.$warning(response.data.message)
         }
@@ -150,7 +181,6 @@ export default {
       window.open(url, '_blank')
     },
     beforeUpload(file) {
-      // console.log(file)
       this.importLic(file)
       return false
     }
@@ -166,7 +196,7 @@ export default {
         flex-direction: row;
         width: 640px;
         min-width: 640px;
-        height: 400px;
+        height: 450px;
         position: relative;
         >>>div.el-card__header {
             padding: 0;
@@ -182,18 +212,29 @@ export default {
 
     .license-content {
         font-size: 16px;
-        padding: 50px;
-        >>>table {
-            width: 100%;
+        padding: 0px 20px;
+        
+        .col_box {
+          margin-bottom: 10px;
+
+          .box_title {
+            text-align: right;
+            padding-right: 5px;
+            font-weight: bold;
+          }
         }
-        >>>th {
-            text-align: left;
-            width: 45%;
-        }
-        >>>td {
-            display: table-cell;
-            vertical-align: inherit;
-        }
+
+        // >>>table {
+        //     width: 100%;
+        // }
+        // >>>th {
+        //     text-align: left;
+        //     width: 45%;
+        // }
+        // >>>td {
+        //     display: table-cell;
+        //     vertical-align: inherit;
+        // }
     }
     .md-padding {
         padding: 10px;
