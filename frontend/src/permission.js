254646210238c048084eb533ae0c4b98b4b7e15e
thyme-vv <thyme_v@163.com>
2025-03-04 16:38:25 +0800

    修改pc数据填报路由

--- frontend/src/permission.js
+++ frontend/src/permission.js
@@ -95,85 +95,85 @@ router.beforeEach(async(to, from, next) => {
 })
 export const loadMenus = (next, to) => {
   buildMenus().then(res => {
-    const datas = res.data
-    // const datas = [...res.data, {
-    //   "path": "/data-filling",
-    //   "component": "Layout",
-    //   "redirect": "/data-filling/my-jobs",
-    //   "name": "data-filling",
-    //   "meta": {
-    //     "title": "数据填报",
-    //     "icon": ""
-    //   },
-    //   "pid": 0,
-    //   "id": 101,
-    //   "permission": null,
-    //   "hidden": false,
-    //   "type": 0,
-    //   "menuSort": 10,
-    //   "isPlugin": false,
-    //   "noLayout": null,
-    //   "children": [
-    //     {
-    //       "path": "my-jobs",
-    //       "component": "dataFilling/myTask/index",
-    //       "redirect": null,
-    //       "name": "my-jobs",
-    //       "meta": {
-    //         "title": "我的填报",
-    //         "icon": ""
-    //       },
-    //       "pid": 101,
-    //       "id": 102,
-    //       "permission": "my-data-filling:manage",
-    //       "hidden": false,
-    //       "type": 1,
-    //       "menuSort": 1,
-    //       "isPlugin": false,
-    //       "noLayout": null,
-    //       "children": null
-    //     },
-    //     {
-    //       "path": "forms",
-    //       "component": "dataFilling/index",
-    //       "redirect": null,
-    //       "name": "data-filling-form",
-    //       "meta": {
-    //         "title": "表单管理",
-    //         "icon": ""
-    //       },
-    //       "pid": 101,
-    //       "id": 103,
-    //       "permission": "data-filling-form:manage",
-    //       "hidden": false,
-    //       "type": 1,
-    //       "menuSort": 1,
-    //       "isPlugin": false,
-    //       "noLayout": null,
-    //       "children": null
-    //     },
-    //     {
-    //       "path": "folder",
-    //       "component": "dataFilling/form/create",
-    //       "redirect": null,
-    //       "name": "data-filling-form-create",
-    //       "meta": {
-    //         "title": "创建",
-    //         "icon": ""
-    //       },
-    //       "pid": 101,
-    //       "id": 104,
-    //       "permission": null,
-    //       "hidden": false,
-    //       "type": 1,
-    //       "menuSort": 1,
-    //       "isPlugin": false,
-    //       "noLayout": null,
-    //       "children": null
-    //     },
-        
-    //   ]
-    // }]
+    // const datas = res.data
+    let list = res.data.filter(item => item.path !== "/data-filling")
+    const datas = [...list, {
+      "path": "/data-filling",
+      "component": "Layout",
+      "redirect": "/data-filling/my-jobs",
+      "name": "data-filling",
+      "meta": {
+        "title": "数据填报",
+        "icon": ""
+      },
+      "pid": 0,
+      "id": 101,
+      "permission": null,
+      "hidden": false,
+      "type": 0,
+      "menuSort": 10,
+      "isPlugin": false,
+      "noLayout": null,
+      "children": [
+        {
+          "path": "my-jobs",
+          "component": "dataFilling/index",
+          "redirect": null,
+          "name": "my-jobs",
+          "meta": {
+            "title": "表单管理",
+            "icon": ""
+          },
+          "pid": 101,
+          "id": 103,
+          "permission": "data-filling-form:manage",
+          "hidden": false,
+          "type": 1,
+          "menuSort": 1,
+          "isPlugin": false,
+          "noLayout": null,
+          "children": null
+        },
+        {
+          "path": "forms",
+          "component": "dataFilling/myTask/index",
+          "redirect": null,
+          "name": "data-filling-form",
+          "meta": {
+            "title": "我的填报",
+            "icon": ""
+          },
+          "pid": 101,
+          "id": 102,
+          "permission": "my-data-filling:manage",
+          "hidden": false,
+          "type": 1,
+          "menuSort": 1,
+          "isPlugin": false,
+          "noLayout": null,
+          "children": null
+        },
+        {
+          "path": "folder",
+          "component": "dataFilling/form/create",
+          "redirect": null,
+          "name": "data-filling-form-create",
+          "meta": {
+            "title": "创建",
+            "icon": ""
+          },
+          "pid": 101,
+          "id": 104,
+          "permission": null,
+          "hidden": false,
+          "type": 1,
+          "menuSort": 1,
+          "isPlugin": false,
+          "noLayout": null,
+          "children": null
+        },
+      ]
+    }]
     // console.log('datas', JSON.parse(JSON.stringify(datas)))
     const filterDatas = filterRouter(datas)
     const asyncRouter = filterAsyncRouter(filterDatas)
