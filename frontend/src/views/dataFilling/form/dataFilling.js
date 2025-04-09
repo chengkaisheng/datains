a2bbcc223405cb4c1794b9b4b84a48b398b170ea
thyme-vv <thyme_v@163.com>
2025-04-09 18:38:28 +0800

    修改自主填报下载

--- frontend/src/views/dataFilling/form/dataFilling.js
+++ frontend/src/views/dataFilling/form/dataFilling.js
@@ -85,10 +85,11 @@ export function deleteData(formId, rowId) {
     loading: true
   })
 }
-export function downloadTemplate(formId) {
+export function downloadTemplate(formId,password) {
   return request({
-    url: 'dataFilling/form/' + formId + '/excel/template',
-    method: 'post',
+    // url: 'dataFilling/form/' + formId + '/excel/template', // 导出表单模板
+    url: 'dataFilling/form/' + formId + '/excel/exportExcelData?password=' + password, // 导出表单数据
+    method: 'get',
     loading: true,
     responseType: 'blob'
   })
