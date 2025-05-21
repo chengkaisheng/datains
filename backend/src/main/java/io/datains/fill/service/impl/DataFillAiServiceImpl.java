7c69979d5f1d2ed47bacb9fa1bf53c5fb3b218fd
zihang.zhang <zihang.zhang@zhejintech.com>
2025-05-21 15:09:43 +0800

    fix 组织负责人同步权限错误

--- backend/src/main/java/io/datains/fill/service/impl/DataFillAiServiceImpl.java
+++ backend/src/main/java/io/datains/fill/service/impl/DataFillAiServiceImpl.java
@@ -37,7 +37,7 @@ import java.util.List;
  */
 @Service
 public class DataFillAiServiceImpl implements DataFillAiService {
-    @Value("ai.url")
+    @Value("${ai.url}")
     private String aiUrl;
 
     @Override
